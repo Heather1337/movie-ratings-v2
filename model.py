6299684cb56395f838ea83a8845d5a17ac4d0a86
vagrant <vagrant@vagrant.vm>
2020-10-28 23:54:23 +0000

    Adding Movie and Rating tables.

--- model.py
+++ model.py
@@ -19,6 +19,35 @@ class User(db.Model):
         return f'<User user_id={self.user_id} email={self.email}>'
 
 
+class Movie(db.Model):
+    """A movie."""
+
+    __tablename__ = 'movies'
+
+    movie_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, unique=True)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Movie movie_id={self.movie_id} title={self.title}>'
+
+class Rating(db.Model):
+    """A rating."""
+
+    __tablename__ = 'ratings'
+
+    rating_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    score = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    def __repr__(self):
+        return f'<Rating rating_id={self.rating_id} score={self.score}>'
+        
+
+
 def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     flask_app.config['SQLALCHEMY_ECHO'] = echo
