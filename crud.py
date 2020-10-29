de5e1ffe1cb17d0546e91f28b1163e8755e7a569
vagrant <vagrant@vagrant.vm>
2020-10-29 18:19:13 +0000

    Added User and Movie create function.

--- crud.py
+++ crud.py
@@ -7,3 +7,24 @@ from model import db, User, Movie, Rating, connect_to_db
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
+
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
+def create_movie(title, overview, release_date, poster_path):
+    """Create and return a new movie."""
+
+    movie = Movie(title=title, overview=overview, release_date=release_date,
+                  poster_path=poster_path)
+
+    db.session.add(movie)
+    db.session.commit()
+
+    return movie
